76e27e7a9d6f7ae5ffa427b8d1679649e556e5b9
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2023-02-24 00:44:31 -0300

    Update dogs.py

--- dogs.py
+++ dogs.py
@@ -1,27 +1,32 @@
 import tensorflow as tf
-import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
+
+tf.config.run_functions_eagerly(True)
 
 # Diretório de treinamento e teste
 train_dir = "cachorros/treino"
 test_dir = "cachorros/teste"
 
 # Cria geradores de imagem para dados de treinamento e teste
-train_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
         train_dir,
         target_size=(224, 224),
         batch_size=32,
-        class_mode='categorical')
+        class_mode='categorical',
+        classes=['poodle', 'pug']
+        )
 
 validation_generator = test_datagen.flow_from_directory(
         test_dir,
         target_size=(224, 224),
         batch_size=32,
-        class_mode='categorical')
+        class_mode='categorical',
+        classes=['poodle', 'pug']
+        )
+
+tf.keras.backend.clear_session()
 
 # Define o modelo
 model = tf.keras.models.Sequential([
@@ -35,29 +40,39 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Dense(4, activation='softmax')
 ])
 
 # Compila o modelo
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'], run_eagerly=True)
 
 # Treina o modelo
 history = model.fit(
       train_generator,
       steps_per_epoch=train_generator.samples // train_generator.batch_size,
-      epochs=13,
+      epochs=4,
       validation_data=validation_generator,
       validation_steps=validation_generator.samples // validation_generator.batch_size)
 
 # Avalia a acurácia do modelo com dados de teste
-test_loss, test_acc = model.evaluate(validation_generator, verbose=2)
+test_loss, test_acc = model.evaluate(validation_generator, verbose=4)
 print('Assertividade:', test_acc)
 
+
+'''
+
+2º PASSO 
+
+CARREGAR E TESTAR
+
+
+import numpy as np
+
 # carrega a imagem
-img = image.load_img('cachorro.jpg', target_size=(224, 224))
+img = tf.keras.preprocessing.image.load_img('cachorro.jpg', target_size=(224, 224))
 
 # converte a imagem em um array numpy
-img_array = image.img_to_array(img)
+img_array = tf.keras.preprocessing.image.img_to_array(img)
 
 # adiciona uma dimensão extra para representar o lote de imagens (batch)
 img_array = np.expand_dims(img_array, axis=0)
@@ -70,4 +85,4 @@ model = tf.keras.models.load_model('modelo_cachorro.h5')
 prediction = model.predict(img_array)
 
 # exibe a previsão
-print('Este é um(a): ',prediction)
+print('Este é um(a): ',prediction)'''
