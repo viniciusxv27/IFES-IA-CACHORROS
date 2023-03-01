2ac3b5c31b70ef4eea9b5cb0700455b0be4e06bb
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2023-03-01 15:02:41 -0300

    Update dogs.py

--- dogs.py
+++ dogs.py
@@ -10,8 +10,6 @@ test_dir = "cachorros/teste/"
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
-print(train_datagen)
-print(test_datagen)
 train_generator = train_datagen.flow_from_directory(
         train_dir,
         target_size=(224, 224),
@@ -58,34 +56,5 @@ assetividade = "±{:.0f}%".format(test_acc*100)
 
 print('Assertividade:', assetividade)
 
-# SALVAR O ARQUIVO (OPCIONAL)
-# model.save("modelo_cachorros.h5")
-
-
-'''
-
-2º PASSO 
-
-CARREGAR E TESTAR
-
-
-import numpy as np
-
-# carrega a imagem
-img = tf.keras.preprocessing.image.load_img('cachorro.jpg', target_size=(224, 224))
-
-# converte a imagem em um array numpy
-img_array = tf.keras.preprocessing.image.img_to_array(img)
-
-# adiciona uma dimensão extra para representar o lote de imagens (batch)
-img_array = np.expand_dims(img_array, axis=0)
-
-# normaliza os valores dos pixels para estarem entre 0 e 1
-img_array /= 255.0
-
-# faz a previsão com o modelo
-model = tf.keras.models.load_model('modelo_cachorro.h5')
-prediction = model.predict(img_array)
-
-# exibe a previsão
-print('Este é um(a): ',prediction)'''
+# Salvar o arquivo ( Opcional ) { Obrigatorio se for testar no "dog_analyze.py" }
+# model.save("modelo_cachorro.h5")
