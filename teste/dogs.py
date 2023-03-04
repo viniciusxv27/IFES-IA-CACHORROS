9ab5d47b48885f75025858a4f87fc94d2d86e7e0
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2023-03-04 01:05:50 -0300

    Add files via upload

---
+++ teste/dogs.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+
+tf.config.run_functions_eagerly(True)
+
+train_dir = "cachorros/treino/"
+test_dir = "cachorros/teste/"
+
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical',
+        classes=['poodle', 'pug']
+        )
+
+validation_generator = test_datagen.flow_from_directory(
+        test_dir,
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical',
+        classes=['poodle', 'pug']
+        )
+
+tf.keras.backend.clear_session()
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'], run_eagerly=True)
+
+history = model.fit(validation_generator, epochs=4)
+
+test_loss, test_acc = model.evaluate(validation_generator, verbose=1)
+
+assetividade = "±{:.0f}%".format(test_acc*100)
+
+print('Assertividade:', assetividade)
+
+model.save("modelo_cachorro.h5")
