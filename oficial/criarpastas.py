4b8eb2cc6afb53090fed32688bb382ae6b5fa756
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2023-03-10 13:47:26 -0300

    Update criarpastas.py

--- oficial/criarpastas.py
+++ oficial/criarpastas.py
@@ -1,48 +1,47 @@
 import requests
 import os
 
+
 def main():
-    
-    #pega os nomes da API de lista
+    qtd = 10
+
     url = 'https://dog.ceo/api/breeds/list/all'
     racas_json = requests.get(url).json()
     racas_json = racas_json['message']
-    print(racas_json)
-    
-    #separa as imagens por nome
+
     for key, value in racas_json.items():
-        
+
         if (value):
             for name_value in value:
-                #pega as imagens de sub raças da API de imagem
-                url = 'https://dog.ceo/api/breed/' + key + '/' + name_value + '/images/random'
+                url = 'https://dog.ceo/api/breed/' + key + '/' + \
+                    name_value + '/images/random/' + str(qtd)
                 img = requests.get(url).json()
-                
-                #baixa a imagem
+
                 download(key, name_value, img)
         else:
-            # pega imagem de raça
-            url = 'https://dog.ceo/api/breed/' + key + '/images/random'
+            url = 'https://dog.ceo/api/breed/' + \
+                key + '/images/random/' + str(qtd)
             img = requests.get(url).json()
 
-            #baixa imgagem
             download(key, '', img)
-            
-#download das raças e sub raças                    
-def download(name, subname, img):
-        url = img['message']
-        if (img['status'] == 'success'):
-            img = requests.get(url)
-            
-            if (subname != ''):
-                subname = '-' + subname
-            
-            if not os.path.exists(name + subname):
-                os.makedirs(name + subname)
-                print('Diretório criado com sucesso.')
-                with open(name + subname  + '/' + name + '.jpg', 'wb') as file:
-                     file.write(img.content)
-                     print('Imagem salva com sucesso.')
 
+def download(name, subname, req):
+    url = req['message']
+    if (req['status'] == 'success'):
+
+        if (subname != ''):
+            subname = '-' + subname
+
+        if (not os.path.exists(name + subname)):
+            os.makedirs(name + subname)
+
+        for link in url:
+            img = requests.get(link)
+
+            name_file = 'https://images.dog.ceo/breeds/' + name + subname + '/'
+            name_file = link[len(name_file)::]
+
+            with open(name + subname + '/' + name_file, 'wb') as file:
+                file.write(img.content)
 
 main()
